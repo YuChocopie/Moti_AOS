3ed8b3d101a516341bca06a682240740d7a6b20c
yuchocopie <pointu341@gmail.com>
2022-04-18 16:56:40 +0900

    feat: android version 30 이상 이미지 대응

--- app/src/main/java/com/ahobsu/moti/presentation/ui/question/MissionViewModel.kt
+++ app/src/main/java/com/ahobsu/moti/presentation/ui/question/MissionViewModel.kt
@@ -1,7 +1,10 @@
 package com.ahobsu.moti.presentation.ui.question
 
+import android.content.ContentResolver
+import android.content.Context
 import android.net.Uri
 import android.util.Log
+import androidx.core.net.toUri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.ahobsu.moti.domain.AnswerUseCase
@@ -16,6 +19,10 @@ import com.ahobsu.moti.presentation.ui.question.model.MissionItemModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
 
 
 class MissionViewModel(
@@ -42,7 +49,7 @@ class MissionViewModel(
     val complete: LiveData<Unit> = _complete
 
     private val _getImage = MutableLiveData<Unit>()
-    val getImage: LiveData<Unit>  =_getImage
+    val getImage: LiveData<Unit> = _getImage
 
     fun initMission() {
         getMissions()
@@ -52,8 +59,8 @@ class MissionViewModel(
         _getImage.value = Unit
     }
 
-    fun onClickComplete() {
-        postAnswer()
+    fun onClickComplete(context: Context) {
+        postAnswer(context)
     }
 
     fun onClickBack() {
@@ -107,12 +114,19 @@ class MissionViewModel(
             })
     }
 
-    private fun postAnswer() {
+    private fun postAnswer(context: Context) {
         selectMission.value?.id?.let {
             AnswerUseCase(answerRepository).postAnswer(
                 Answer(
                     content = missionAnswer.value?.content,
-                    file = missionAnswer.value?.file,
+                    file =
+                    if (missionAnswer.value?.file == null) null
+                    else createCopyAndReturnRealPath(
+                        context,
+                        missionAnswer.value?.file!!,
+                        "photo.jpg"
+                    ),
+//                    file = missionAnswer.value?.file,
                     missionId = it
                 )
             ).subscribeOn(Schedulers.io())
@@ -123,6 +137,33 @@ class MissionViewModel(
                     Log.e("e", e.toString())
                 })
         }
+    }
+
+    private val tempFileList: ArrayList<String> = ArrayList()
+
+    fun createCopyAndReturnRealPath(context: Context, uri: Uri, fileName: String): Uri? {
+        val contentResolver: ContentResolver = context.contentResolver ?: return null
+        // 내부 저장소 안에 위치하도록 파일 생성
+        val filePath: String =
+            context.applicationInfo.dataDir + File.separator + System.currentTimeMillis()
+                .toString() + "." + fileName.substring(fileName.lastIndexOf(".") + 1)
+        val file = File(filePath)
+        // 서버에 이미지 업로드 후 삭제하기 위해 경로를 저장해둠
+        tempFileList.add(filePath)
+        try {
+            // 매개변수로 받은 uri 를 통해  이미지에 필요한 데이터를 가져온다.
+            val inputStream = contentResolver.openInputStream(uri) ?: return null
+            // 가져온 이미지 데이터를 아까 생성한 파일에 저장한다.
+            val outputStream: OutputStream = FileOutputStream(file)
+            val buf = ByteArray(1024)
+            var len: Int
+            while (inputStream.read(buf).also { len = it } > 0) outputStream.write(buf, 0, len)
+            outputStream.close()
+            inputStream.close()
+        } catch (ignore: IOException) {
+            return null
+        }
+        return file.absolutePath.toUri() // 생성한 파일의 절대경로 반환
     }
 
     private fun getMissions() {
